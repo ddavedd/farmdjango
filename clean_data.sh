88ee48159abae158a0036c0da4d46c60b853a27e
David Smits <david.j.smits@gmail.com>
2025-01-22 22:52:54 -0600

    More scripts might not be used

---
+++ clean_data.sh
@@ -0,0 +1,5 @@
+cd /home/farm/farmdjango
+sed -e 's/<[^>]*>//g' year_comparison1.data > clean1.data
+sed -e 's/<[^>]*>//g' year_comparison7.data > clean7.data
+sed '$d' clean1.data > cleaner1.data
+sed '$d' clean7.data > cleaner7.data
---
+++ gnuplot_all.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/farm/farmdjango
+gnuplot comparison.plot
+gnuplot count_week.plot
+gnuplot totals7.plot
